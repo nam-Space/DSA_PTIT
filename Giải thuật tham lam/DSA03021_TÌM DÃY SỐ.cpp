6a0bc0f63491aa5d3575bed8ecb23a7eec267fba
Nam Nguyễn D21 <92786060+nam-Space@users.noreply.github.com>
2024-07-17 15:02:47 +0700

    Update DSA03021_TÌM DÃY SỐ.cpp
    update code for easy to understand

--- Giải thuật tham lam/DSA03021_TÌM DÃY SỐ.cpp
+++ Giải thuật tham lam/DSA03021_TÌM DÃY SỐ.cpp
@@ -1,36 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define quick() ios_base::sync_with_stdio(false); cin.tie(0);
-#define pb push_back
 using ll = long long;
-int mod = 1e9 + 7;
+const int mod = 1e9 + 7;
+
+bool check(int p, int a[], int n) {
+	for (int i = 0; i < n; i++) {
+		if (a[i] / p < (a[i] / (p + 1) + 1)) return false;
+	}
+	return true;
+}
 
 int main(){
-    quick();
-    int n, min_val = 1e9;
-    int s[2][100000 ];
-    cin >> n;
-    memset(s, 0, sizeof(s));
-    for(int i = 0; i < n; i++){
-        int x; cin >> x;
-        min_val = min(min_val, x);
-        s[0][1]++;
-        s[1][1] += x;
-        for(int j = 2; j <= x; j++){
-            if(x / j != x / (j - 1)){
-                s[0][x / j]++;
-                s[1][x / j] += j;
-            }
-        }
-    }
-    for(int i = min_val; i > 0; i--){
-        if(s[0][i] == n){
-            cout << s[1][i];
-            break;
-        }
-    }
+    int t;
+    t = 1;
+    while(t--) {
+    	int n;
+    	cin >> n;
+    	int a[n];
+    	for (int i = 0; i < n; i++) {
+    		cin >> a[i];
+		}
+		int s = *min_element(a, a + n);
+		int res = 0;
+		for (int i = s; i >= 1; i--) {
+			if (check(i, a, n)) {
+				for (int x : a) {
+					res += x / (i + 1) + 1;
+				}
+				break;
+			}
+		}
+		cout << res << endl;
+	}
 }
-/*
-
-*/
